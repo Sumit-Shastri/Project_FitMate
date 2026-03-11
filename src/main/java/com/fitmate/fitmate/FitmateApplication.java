e638814ea55a1ce265c2f908b7582a120a644bb0
Sumit-Shastri <shastrisumit25@gmail.com>
2026-03-11 11:33:38 +0530

    Changes in main file

--- src/main/java/com/fitmate/fitmate/FitmateApplication.java
+++ src/main/java/com/fitmate/fitmate/FitmateApplication.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FitmateApplication {
-
+public class FitmateApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(FitmateApplication.class, args);
 	}
-
 }
