6a09348945851ba9e90508395c535a8bb2ef04c8
Sumit-Shastri <shastrisumit25@gmail.com>
2026-03-11 17:31:30 +0530

    User Password encryption using Bcrypt

--- src/main/java/com/fitmate/fitmate/service/AuthService.java
+++ src/main/java/com/fitmate/fitmate/service/AuthService.java
@@ -3,6 +3,7 @@ package com.fitmate.fitmate.service;
 import com.fitmate.fitmate.model.User;
 import com.fitmate.fitmate.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,8 +12,13 @@ public class AuthService
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public User signup(User user)
     {
+        // encrypt password
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 }
